4af06b93a84a11cd8c713373bb36bc86e647e588
Eros Campos <eroscamposg@gmail.com>
2021-02-10 01:56:42 -0500

    inital commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container mx-auto text-center">
+    <h1 class="text-6xl">About Dad Jokes</h1>
+    <p>This is an app that displays dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: "About the App",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
